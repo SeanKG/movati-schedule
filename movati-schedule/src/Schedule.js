930494785bd654131b8b031a049ef40cfd743fab
Sean Graves <seankgraves@gmail.com>
2018-11-13 15:12:27 -0500

    only displaying one day of data

--- movati-schedule/src/Schedule.js
+++ movati-schedule/src/Schedule.js
@@ -22,6 +22,7 @@ import { timelines } from 'd3-timelines';
 function scheduleToTimeline( sched ){
 	return {
 		// class: "pA",
+		sched,
 		label: sched.room,
 		times: [{
 			"starting_time": sched.startTime,
@@ -30,6 +31,17 @@ function scheduleToTimeline( sched ){
 		}
 }
 
+function timeFilter( sched ){
+	const now = new Date();
+	now.setHours(6);
+	now.setMinutes(0);
+	const later = new Date();
+	later.setHours(21);
+	later.setMinutes(0);
+	// const later = now + (1000 * 60 * 60 * 24 * 2);
+
+	return sched.startTime > now.getTime() && sched.endTime < later.getTime();
+}
 
 class Schedule extends Component {
 	
@@ -40,26 +52,36 @@ class Schedule extends Component {
 	}
 	componentDidUpdate() {
 		this.buildChart();
-		console.log(this.props.schedules);
 	}
 
 	buildChart = () =>{
 		const { schedules } = this.props;
 		if( schedules ){
 			const data = schedules
-							.filter(s => s.location === "Trainyards")
-							.map(scheduleToTimeline);
+				.filter(s => s.location === "Trainyards" && s.room === "Co-ed Studio")
+				.filter(timeFilter)
+				.map(scheduleToTimeline);
+				console.log(data);
 			let chart = timelines();
 			d3.select(this.svg)
-				.attr("width", 500)
-				.attr("height", 500)
+				.attr("width", window.innerWidth)
+				.attr("height", 300)
 				.datum(data).call(chart);
+
+			// this.setState({ data });
 		}
 	}
 
 	render() {
 		return (
-			<svg ref={node => this.svg = node}></svg>
+			<div>
+				<svg ref={node => this.svg = node}></svg>
+				<pre className="App-header">
+					{ this.state.schedules ? JSON.stringify(this.state.schedules, null, 2) : `Mervati` }
+				</pre>
+				
+			</div>
+  
 		);
 	}
 
